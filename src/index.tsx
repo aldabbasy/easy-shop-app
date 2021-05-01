b5fe9c97367d1371598014b5b3abc2d39deb31b4
AL-Laith Dababseh <45473839+aldabbasy@users.noreply.github.com>
2021-05-01 07:52:39 +0300

    added darkmode

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import App from './app/App';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core';
+
+const lightTheme = createMuiTheme({
+  palette: {
+    type: 'light'
+  },
+});
+
+const darkTheme = createMuiTheme({
+  palette: {
+    type: 'dark'
+  },
+});
 
 ReactDOM.render(
-    <>
+    <MuiThemeProvider theme={darkTheme ? darkTheme : lightTheme}>
       <CssBaseline />
       <App />
-    </>,
+    </MuiThemeProvider>,
   document.getElementById('root')
 );
