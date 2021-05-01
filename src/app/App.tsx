d05fe37b85c3c2a25717e93ce7f1806d8f9b5f37
AL-Laith Dababseh <45473839+aldabbasy@users.noreply.github.com>
2021-05-01 07:57:14 +0300

    moved darkmode setub to app.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import Login from '../pages/Login'
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core';
+
+
+const lightTheme = createMuiTheme({
+  palette: {
+    type: 'light'
+  },
+});
+
+const darkTheme = createMuiTheme({
+  palette: {
+    type: 'dark'
+  },
+});
 
 const App = () => {
   return (
-    <Login />
+    <MuiThemeProvider theme={darkTheme ? darkTheme : lightTheme}>
+      <CssBaseline />
+      <Login />
+    </MuiThemeProvider>
   )
 }
 
