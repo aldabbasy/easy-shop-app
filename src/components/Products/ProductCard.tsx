1fbbf00f996bc0aa8c4ef4e88b6964891b091465
AL-Laith Dababseh <45473839+aldabbasy@users.noreply.github.com>
2021-05-06 16:19:20 +0300

    minor design improvments

--- src/components/Products/ProductCard.tsx
+++ src/components/Products/ProductCard.tsx
@@ -6,6 +6,7 @@ import CardHeader from '@material-ui/core/CardHeader';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
+import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
@@ -16,10 +17,13 @@ import { makeStyles } from '@material-ui/core';
 const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: '100%',
-    maxHeight: 250,
+    maxHeight: 350,
   },
   actionBtns: {
     marginLeft: 'auto',
+  },
+  media: {
+    height: 100,
   },
 }));
 
@@ -39,6 +43,11 @@ const ProductCard = ({product}) => {
                 <ChevronRightOutlinedIcon />  
               </IconButton>
             }
+          />
+          <CardMedia
+            className={classes.media}
+            image={`https://picsum.photos/200?rand=${product?.name}`}
+            title={product?.name}
           />
           <CardContent>
             <Typography variant="body2" color="textSecondary" component="p">
